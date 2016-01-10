3bb0fb1813073790ddc2aa70f4ac0aa7e65789a0
David <davidmedenjak@gmail.com>
2016-01-10 23:50:38 +0100

    added dagger module generation

--- sharedpreferencesannotationsprocessor/src/main/java/com/github/bleeding182/sharedpreferences/annotations/processor/SharedPreferenceManager.java
+++ sharedpreferencesannotationsprocessor/src/main/java/com/github/bleeding182/sharedpreferences/annotations/processor/SharedPreferenceManager.java
@@ -6,9 +6,12 @@ import com.squareup.javawriter.JavaWriter;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.JavaFileObject;
 
@@ -33,11 +36,51 @@ public class SharedPreferenceManager {
             JavaWriter mWriter = new JavaWriter(jfo.openWriter());
             PreferenceClassWriter writer = new PreferenceClassWriter(preference, mWriter);
             writer.write();
+
+            writeModule(filer, preference);
+
         }
         mPreferences.clear();
     }
 
-    public int getCount() {
-        return mPreferences.size();
+    private void writeModule(Filer filer, Preference preference) throws IOException {
+        try {
+            Class.forName("dagger.Module");
+        } catch (ClassNotFoundException e) {
+            // no dagger present
+            return;
+        }
+
+        final String type = preference.getType().getSimpleName() + "Module";
+        JavaFileObject jfo = filer.createSourceFile(preference.getPackage() + "." + type);
+        JavaWriter writer = new JavaWriter(jfo.openWriter());
+        writer.setIndent("    ");
+
+
+        Set<Modifier> pubModifier = new LinkedHashSet<>();
+        pubModifier.add(Modifier.PUBLIC);
+        writer.emitPackage(preference.getPackage());
+
+        writer.emitImports("dagger.Module", "dagger.Provides");
+        writer.emitEmptyLine();
+
+        writer.emitAnnotation("dagger.Module");
+        writer.beginType(type, "class", pubModifier);
+        writer.emitEmptyLine();
+
+        writer.emitAnnotation("dagger.Provides");
+        writer.beginMethod(preference.getType().getSimpleName().toString(),
+                "provide" + preference.getType().getSimpleName(),
+                pubModifier, preference.getImplementationName(), "preferences");
+
+        writer.emitStatement("return %s", "preferences");
+        writer.endMethod();
+
+        writer.emitEmptyLine();
+
+        writer.endType();
+        writer.close();
+
+
     }
 }
